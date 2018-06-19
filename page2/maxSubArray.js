e20840db55f38bc87274a2f4e77a262975c968e7
shaku <shaku@iij.ad.jp>
2018-06-19 15:01:59 +0900

    leet

--- page2/maxSubArray.js
+++ page2/maxSubArray.js
@@ -8,5 +8,11 @@
  * @return {number}
  */
 var maxSubArray = function(nums) {
-  
+  let currMax = nums[0];
+  let max = nums[0];
+  for (let i = 1; i < nums.length; i ++) {
+    currMax = Math.max(nums[i] + currMax, nums[i]);
+    max = Math.max(currMax, max);
+  }
+  return max;
 };
---
+++ page2/spiralOrder.js
@@ -0,0 +1,19 @@
+/**
+* Spiral Matrix
+* https://leetcode.com/problems/spiral-matrix/description/
+*/
+
+/**
+ * @param {number[][]} matrix
+ * @return {number[]}
+ */
+var spiralOrder = function(matrix) {
+  if (matrix.length > 1) {
+    
+  }
+
+};
+
+let arr = [[7],[9],[6]]
+
+console.log(spiralOrder(arr));
