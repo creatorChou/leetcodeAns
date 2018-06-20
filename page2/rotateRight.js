add3775138d2d35b65697336d1197175a0796c14
Zhou Shuo <523547335@qq.com>
2018-06-20 22:34:07 +0900

    leet

---
+++ page2/rotateRight.js
@@ -0,0 +1,40 @@
+/**
+*	Rotate List
+*	https://leetcode.com/problems/rotate-list/description/
+*/
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} k
+ * @return {ListNode}
+ */
+var rotateRight = function(head, k) {
+	let node = head;
+	let newHead = null;
+	let foot = null;
+	let len = 0;
+  while (node) {
+  	if (!node.next) {
+  		foot = node;
+  	}
+		len ++;
+  	node = node.next;
+  }
+
+  let l = len - k % len;
+  node = head;
+  for (let i = 0; i < len; i ++) {
+  	node = node.next;
+  }
+  newHead = node.next;
+  node.next = null;
+  foot.next = head;
+  return newHead;
+};
