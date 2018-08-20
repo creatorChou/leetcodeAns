d3b44ba4e54aaf221bc6654de638e731704dc8dc
Zhou Shuo <523547335@qq.com>
2018-08-20 23:34:03 +0900

    leet

--- page7/removeInvalidParentheses.js
+++ page7/removeInvalidParentheses.js
@@ -23,63 +23,92 @@ var removeInvalidParentheses = function(s) {
       break;
     }
   }
+  s = s.join("");
   if (isValid(s)) {
-    return [s.join("")];
+    return [s];
   }
-  let left = [], right = [];
+  let left = 0, right = 0, prev, middle, after, j, k, str;
   for (let i = 0; i < s.length; i ++) {
-    if (s[i] === "(") {
-      left.push(i);
-    } else if (s[i] === ")") {
-      right.push(i);
+    if (s[i] === "(") left ++;
+    if (s[i] === ")") right ++;
+  }
+  let result = [], max = 0;
+  if (left <= right) {
+    let start = s.indexOf("(");
+    if (start === -1) {
+      result.push(s.replace(/\)/g, ""));
+    } else {
+      prev = s.slice(0, start);
+      for (let i = start + 1; i < s.length; i ++) {
+        if (s[i] === ")") {
+          middle = removeInvalidParentheses(s.slice(start + 1, i));
+          after = removeInvalidParentheses(s.slice(i + 1));
+          for (j = 0; j < middle.length; j ++) {
+            for (k = 0; k < after.length; k ++) {
+              str = prev + "(" + middle[j] + ")" + after[k];
+              if (str.length > max) {
+                result = [str];
+                max = str.length;
+              } else if (str.length === max) {
+                if (result.indexOf(str) === -1) {
+                  result.push(str);
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  } else if (left > right) {
+    let end = -1;
+    for (let i = s.length - 1; i >= 0; i --) {
+      if (s[i] === ")") {
+        end = i;
+        break;
+      }
+    }
+    if (end === -1) {
+      result.push(s.replace(/\(/g, ""));
+    } else {
+      after = s.slice(end + 1);
+      for (let i = end - 1; i >= 0; i --) {
+        if (s[i] === "(") {
+          prev = removeInvalidParentheses(s.slice(0, i));
+          middle = removeInvalidParentheses(s.slice(i + 1, end));
+          for (j = 0; j < prev.length; j ++) {
+            for (k = 0; k < middle.length; k ++) {
+              str = prev[j] + "(" + middle[k] + ")" + after;
+              if (str.length > max) {
+                result = [str];
+                max = str.length;
+              } else if (str.length === max) {
+                if (result.indexOf(str) === -1) {
+                  result.push(str);
+                }
+              }
+            }
+          }
+        }
+      }
     }
   }
-  let result = [],
-      choose = [],
-      diff = Math.abs(left.length - right.length),
-      arr = left.length > right.length ? left : right;
-
-  helper(result, choose, diff, arr, s);
   return result;
 };
 
-function helper(result, choose, diff, arr, s) {
-  console.log(choose)
-  if (choose.length === diff) {
-    let ss = s.slice();
-    for (let i = 0; i < choose.length; i ++) {
-      ss[choose[i]] = "";
-    }
-    ss = ss.join("");
-    if (isValid(ss) && result.indexOf(ss) === -1) {
-      result.push(ss);
-    }
-    return;
-  }
-  for (let i = 0; i < arr.length; i ++) {
-    if (choose.indexOf(arr[i]) === -1) {
-      choose.push(arr[i]);
-      helper(result, choose, diff, arr, s);
-      choose.pop();
-    }
-  }
-}
-
-function isValid (s) {
-  let stack = [];
+function isValid(s) {
+  let count = 0;
   for (let i = 0; i < s.length; i ++) {
-    if (s[i] === "(") {
-      stack.push("(");
-    }
-    if (s[i] === ")") {
-      if (stack.length > 0) {
-        stack.pop();
+    if(s[i] === "(") {
+      count ++;
+    } else if (s[i] === ")") {
+      if (count > 0) {
+        count --;
       } else {
         return false;
       }
     }
   }
-  return stack.length === 0;
+  return count === 0;
 }
 
-console.log(removeInvalidParentheses("())(((()m)("))
+console.log(removeInvalidParentheses("((()))((()(()"))
