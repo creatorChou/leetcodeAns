c64b6e39fb46fc36a645f6ba603652d7be447ea2
Zhou Shuo <523547335@qq.com>
2018-09-03 00:13:38 +0900

    leet

--- page7/isAdditiveNumber.js
+++ page7/isAdditiveNumber.js
@@ -8,9 +8,21 @@
  * @return {boolean}
  */
 var isAdditiveNumber = function(num) {
-
+  let len = num.length;
+  for (let i = 1; i <= len / 2; i ++) {
+    for (let j = 1; Math.max(i, j) >= len - i - j; j ++) {
+      if (isValid(i, j, num)) return true;
+    }
+  }
+  return false;
 };
 
-function helper (num1, num) {
-
+function isValid (i, j, num) {
+  if (num.charAt(0) === "0" && i > 1) return false;
+  if (num.charAt(i) === "0" && j > 1) return false;
+  let first = Number(num.substr(0, i)),
+      second = Number(num.substr(i, j)),
+      sum = first + second + "";
+  for (let start = i + j; start < num.length; start += sum.length) {
+  }
 }
