626914219b670e969f3f193b15b54c106a5e6d71
Zhou Shuo <523547335@qq.com>
2018-09-05 22:07:52 +0900

    leet

--- page7/coinChange.js
+++ page7/coinChange.js
@@ -8,10 +8,53 @@
  * @param {number} amount
  * @return {number}
  */
-var coinChange = function(coins, amount) {
-
+ // recursive
+var coinChange = function(coins, amount, count) {
+	if (amount === 0) return 0;
+	let min = null,
+			after;
+	if (!count) {
+		count = [];
+	}
+	if (count[amount] != null) {
+		return count[amount];
+	}
+	for (let i = 0; i < coins.length; i ++) {
+		if (amount > coins[i]) {
+			after = coinChange(coins, amount - coins[i], count);
+			if (after !== -1) {
+				min = min === null ? after : Math.min(min, after);
+			}
+		} else if (amount === coins[i]) {
+			return 1;
+		}
+	}
+	if (min === null) {
+		count[amount] = -1;
+	} else {
+		count[amount] = min + 1;
+	}
+	return count[amount];
 };
 
-console.log(coinChange([186,419,83,408], 6249));
+// console.log(coinChange([186,419,83,408], 6249));
 // [186,419,83,408]
 // 6249
+
+function coinChange2 (coins, amount) {
+	if (amount === 0) return 0;
+	let dp = [],
+			sum = 1;
+	dp[0] = 0;
+	while (sum <= amount) {
+		for (let i = 0; i < coins.length; i ++) {
+			if (sum >= coins[i] && dp[sum - coins[i]] != null && dp[sum - coins[i]] !== -1) {
+				dp[sum] = dp[sum] == null ? dp[sum - coins[i]] + 1 : Math.min(dp[sum], dp[sum - coins[i]] + 1);
+			}
+		}
+		sum ++;
+	}
+	return dp[amount] || -1;
+}
+
+console.log(coinChange2([186,419,83,408], 6249));
