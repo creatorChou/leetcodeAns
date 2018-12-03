0669b1fdb2b568811b6294a67f2aee9476129790
shaku <shaku@iij.ad.jp>
2018-12-03 17:37:50 +0900

    leet

---
+++ page12/smallestRange.js
@@ -0,0 +1,12 @@
+/**
+* 632. Smallest Range
+* https://leetcode.com/problems/smallest-range/
+*/
+
+/**
+ * @param {number[][]} nums
+ * @return {number[]}
+ */
+var smallestRange = function(nums) {
+  
+};
