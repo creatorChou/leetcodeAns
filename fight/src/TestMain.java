95a306f0bea49c24cce9711878dc3279fb9f459e
shaku <shaku@iij.ad.jp>
2019-06-07 16:47:54 +0900

    a

--- fight/src/TestMain.java
+++ fight/src/TestMain.java
@@ -23,6 +23,7 @@ public class TestMain {
     System.out.println("QuickFindUF:" + Arrays.toString(quickFindUF.id));
   }
 
+
   private static void tryQuickUnionUF() {
     QuickUnionUF quickUnionUF = new QuickUnionUF(10);
     quickUnionUF.union(5, 6);
